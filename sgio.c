a56fc946225963812a234762b655895aa3a38a67
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-29 07:47:02 +0200

    Reformat

--- sgio.c
+++ sgio.c
@@ -13,8 +13,8 @@
 #include <unistd.h>
 
 typedef struct {
-	uint64_t flags;
-	int fd;
+    uint64_t flags;
+    int fd;
 } sgio_magic_t;
 
 static sgio_magic_t _sgiom[1];
@@ -22,77 +22,90 @@ static sgio_magic_t _sgiom[1];
 static int
 add_sgio(int fd)
 {
-	return -1;
+    return -1;
 }
 
 static int
 rem_sgio(int fd)
 {
-	return -1;
+    return -1;
 }
 
 static sgio_magic_t *
 lookup_sgio(int fd)
 {
-	return NULL;
+    return NULL;
+}
+
+static bool
+sgio_capable(const char *path)
+{
+    return false;
 }
 
 #define WRAPSYSCALL(ptr, name) \
-        if (ptr == NULL) ptr = dlsym(RTLD_NEXT, name); \
-        if (ptr == NULL) return -1;
+    if (ptr == NULL) ptr = dlsym(RTLD_NEXT, name); \
+    if (ptr == NULL) return -1;
 
 int
 open(const char *path, int flags, ...)
 {
-        static int (*open_)(const char *path, int flags, ...);
+    static int (*open_)(const char *path, int flags, ...);
 
-        WRAPSYSCALL(open_, "open");
+    WRAPSYSCALL(open_, "open");
 
-	return -1;
+    /* XXX Add ... handler for O_CREAT */
+    int fd = open_(path, flags);
+
+    if (sgio_capable(path)) {
+        add_sgio(fd);
+    }
+
+    return fd;
 }
 
 int
 close(int fd)
 {
-        static int (*close_)(int) = NULL;
+    static int (*close_)(int) = NULL;
 
-        WRAPSYSCALL(close_, "close");
+    WRAPSYSCALL(close_, "close");
 
-	return -1;
+    return -1;
 }
 
 ssize_t
 pread(int fd, void *buf, size_t nbyte, off_t offset)
 {
-	return -1;
+    return -1;
 }
 
 ssize_t
 read(int fd, void *buf, size_t nbyte)
 {
-        return -1;
+    return -1;
 }
 
 ssize_t
 readv(int fd, const struct iovec *iov, int iovcnt)
 {
-        return -1;
+    return -1;
 }
 
 ssize_t
 pwrite(int fd, const void *buf, size_t nbyte, off_t offset)
 {
-        return -1;
+    return -1;
 }
 
 ssize_t
 write(int fd, const void *buf, size_t nbyte)
 {
-        return -1;
+    return -1;
 }
 
 ssize_t
 writev(int fd, const struct iovec *iov, int iovcnt)
 {
-        return -1;
+    return -1;
 }
