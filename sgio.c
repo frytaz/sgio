0456535e589d7d45c61b0f71215b6c399c3a31c1
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-29 08:01:54 +0200

    more headers

--- sgio.c
+++ sgio.c
@@ -6,9 +6,11 @@
 #define _GNU_SOURCE
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/uio.h>
 #include <dlfcn.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <unistd.h>
 
